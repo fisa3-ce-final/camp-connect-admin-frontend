918b47c12af5cd55818964bede0b1d4efe73193a
recoild <cajhyung@gmail.com>
2024-10-30 14:19:48 +0900

    init

---
+++ app/coupons/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface PageProps {}
+
+const CouponPage: FC<PageProps> = async ({}) => {
+    return <div>Coupon Page</div>;
+};
+
+export default CouponPage;
---
+++ app/rentals/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface PageProps {}
+
+const RentalPage: FC<PageProps> = async ({}) => {
+    return <div>Rental Page</div>;
+};
+
+export default RentalPage;
